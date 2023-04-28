c9f6618ca8aa38519b496cf937b4ce68ea23fdc2
Chris <chris.suozzo@carmasys.com>
2023-04-28 11:53:29 -0400

    led-roulette challenge

--- f3discovery/src/05-led-roulette/src/main.rs
+++ f3discovery/src/05-led-roulette/src/main.rs
@@ -2,14 +2,23 @@
 #![no_main]
 #![no_std]
 
-use aux5::entry;
+use aux5::{entry, Delay, DelayMs, LedArray, OutputSwitch};
 
 #[entry]
 fn main() -> ! {
-    let _y;
-    let x = 42;
-    _y = x;
+    let (mut delay, mut leds) = aux5::init();
 
-    // infinite loop; just so we don't leave this stack frame
-    loop {}
+    let led_count = leds.len();
+    loop {
+        for i in 0..led_count {
+            leds[i].on().ok();
+            delay.delay_ms(50_u16);
+            if i == 0 {
+                leds[led_count - 1].off().ok();
+            } else {
+                leds[i - 1].off().ok();
+            }
+            delay.delay_ms(50_u16);
+        }
+    }
 }
